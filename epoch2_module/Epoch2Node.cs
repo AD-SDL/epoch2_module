b60267d1ce9bebdb2659483e7c9bdc687fc6a7c2
Dozgulbas <dozgulbas@anl.gov>
2024-04-04 15:09:14 -0500

    Initial commit

---
+++ epoch2_module/Epoch2Node.cs
@@ -0,0 +1,86 @@
+﻿using Grapevine;
+using McMaster.Extensions.CommandLineUtils;
+using Gen5;
+using BTIAUTOSTACKERLib;
+
+namespace epoch2_module
+{
+    class Epoch2Node
+    {
+
+        public static int Main(string[] args) => CommandLineApplication.Execute<Epoch2Node>(args);
+
+        [Option(Description = "Server Hostname")]
+        public string Hostname { get; set; } = "+";
+
+        [Option(Description = "Server Port")]
+        public int Port { get; } = 2000;
+
+        [Option(Description = "Whether or not to simulate the instrument (note: if the instrument is connected, this does nothing)")]
+        public bool Simulate { get; } = true;
+
+        public string state = ModuleStatus.INIT;
+        private IRestServer server;
+
+        private Gen5.Application gen5;
+        private BTIAutoStacker bTIAutoStacker;
+        private short stackerComPort = 2;
+        private short epochReaderType = 1;
+        private short readerComPort = 3;
+        private int readerBaudRate = 3600;
+
+
+        ~Epoch2Node()
+        {
+            Console.WriteLine("Exiting...");
+            //client.Disconnect();
+            //client.Close();
+            server.Stop();
+            bTIAutoStacker.CloseComPort();
+        }
+
+        private void OnExecute()
+        {
+
+            InitializeEpoch2();
+            InitializePlateStacker();
+
+            server = RestServerBuilder.UseDefaults().Build();
+            string server_url = "http://" + Hostname + ":" + Port.ToString() + "/";
+            Console.WriteLine(server_url);
+            server.Prefixes.Clear();
+            server.Prefixes.Add(server_url);
+            server.Locals.TryAdd("state", state);
+            //server.Locals.TryAdd("client", client);
+            try
+            {
+                //server.Start();
+                Console.WriteLine("Press enter to stop the server");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void InitializeEpoch2()
+        {
+            gen5 = new Gen5.Application();
+            gen5.ConfigureSerialReader(epochReaderType, readerComPort, readerBaudRate);
+            Console.WriteLine(gen5.TestReaderCommunication());
+            gen5.CarrierIn();
+            gen5.CarrierOut();
+        }
+
+        private void InitializePlateStacker()
+        {
+            bTIAutoStacker = new BTIAutoStacker();
+            bTIAutoStacker.SetComPort(stackerComPort);
+            bTIAutoStacker.OpenComPort(stackerComPort);
+            Console.WriteLine(bTIAutoStacker.TestCommunicationWithoutDialog());
+            bTIAutoStacker.HomeAllAxes();
+            //bTIAutoStacker.PresentPlateOnCarrier();
+        }
+    }
+}
---
+++ epoch2_module/Epoch2RestServer.cs
@@ -0,0 +1,97 @@
+﻿using Grapevine;
+using Newtonsoft.Json;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace epoch2_module
+{
+    public class Epoch2RestServer
+    {
+
+        private readonly IRestServer _server;
+        public Epoch2RestServer(IRestServer server)
+	    {
+            _server = server;
+	    }
+
+        [RestRoute("Get", "/state")]
+        public async Task State(IHttpContext context)
+        {
+            string state = _server.Locals.GetAs<string>("state");
+            Dictionary<string, string> response = new Dictionary<string, string>
+            {
+                ["State"] = state
+            };
+            Console.WriteLine(state);
+            await context.Response.SendResponseAsync(JsonConvert.SerializeObject(response));
+        }
+
+        [RestRoute("Get", "/about")]
+        public async Task About(IHttpContext context)
+        {
+            // TODO
+            await context.Response.SendResponseAsync(@"
+                {
+                    ""name"":""Epoch2"",
+                    ""model"":""BioTek Epoch2 Plate Reader"",
+                    ""interface"":""wei_rest_node"",
+                    ""version"":""0.1.0"",
+                    ""description"":""Module for automating the Epoch 2 platereader."",
+                    ""actions"": [
+                        {""name"":""open"",""args"":[],""files"":[]},
+                        {""name"":""close"",""args"":[],""files"":[]},
+                        {""name"":""run_assay"",""args"":[{""name"":""assay_name"",""type"":""str"",""default"":null,""required"":true,""description"":""Name of the assay to run""}],""files"":[]}
+                    ],
+                    ""resource_pools"":[]
+                }"
+            );
+        }
+
+        [RestRoute("Get", "/resources")]
+        public async Task Resources(IHttpContext context)
+        {
+            // TODO
+            await context.Response.SendResponseAsync("resources");
+        }
+
+        [RestRoute("Post", "/action")]
+        public async Task Action(IHttpContext context)
+        {
+            string action_handle = context.Request.QueryString["action_handle"];
+            string action_vars = context.Request.QueryString["action_vars"];
+            Dictionary<string, string> args = JsonConvert.DeserializeObject<Dictionary<string, string>>(action_vars);
+            var result = UtilityFunctions.action_response();
+            string state = _server.Locals.GetAs<string>("state");
+            //HidexSenseAutomationServiceClient client = _server.Locals.GetAs<HidexSenseAutomationServiceClient>("client");
+
+            if (state == ModuleStatus.BUSY)
+            {
+                result = UtilityFunctions.action_response(StepStatus.FAILED, "", "Module is Busy");
+                await context.Response.SendResponseAsync(JsonConvert.SerializeObject(result));
+            }
+            try
+            {
+                _server.Locals.TryUpdate("state", ModuleStatus.BUSY, _server.Locals.GetAs<string>("state"));
+                switch (action_handle)
+                {
+                    case "blah":
+                        break;
+                    default:
+                        Console.WriteLine("Unknown action: " + action_handle);
+                        result = UtilityFunctions.action_response(StepStatus.FAILED, "", "Unknown action: " + action_handle);
+                        break;
+                }
+                _server.Locals.TryUpdate("state", ModuleStatus.IDLE, _server.Locals.GetAs<string>("state"));
+            }
+            catch (Exception ex)
+            {
+                _server.Locals.TryUpdate("state", ModuleStatus.ERROR, _server.Locals.GetAs<string>("state"));
+                Console.WriteLine(ex.ToString());
+                result = UtilityFunctions.action_response(StepStatus.FAILED, "", "Step failed: " + ex.ToString());
+            }
+
+            await context.Response.SendResponseAsync(JsonConvert.SerializeObject(result));
+        }
+    }
+
+}
